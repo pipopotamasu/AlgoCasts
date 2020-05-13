93b17f8f68f24015318df956ca78a5f934f15760
pipopotamasu <ec0156hx@gmail.com>
2020-05-13 16:52:48 +0900

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const arr = str.split(' ');
+  return arr.map((chars, i) => {
+    return chars[0].toUpperCase() + chars.slice(1)
+  }).join(' ')
+}
 
 module.exports = capitalize;
