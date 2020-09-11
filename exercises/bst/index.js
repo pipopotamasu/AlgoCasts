a0616213ad6fd9a82185f1dbb81b63d10a7a6adf
pipopotamasu <ec0156hx@gmail.com>
2020-09-11 21:03:04 +0900

    insert

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,28 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert (data) {
+    if (data > this.data) {
+      if (this.right) {
+        this.right.insert(data);
+      } else {
+        this.right = new Node(data);
+      }
+    } else {
+      if (this.left) {
+        this.left.insert(data);
+      } else {
+        this.left = new Node(data);
+      }
+    }
+  }
+}
 
 module.exports = Node;
